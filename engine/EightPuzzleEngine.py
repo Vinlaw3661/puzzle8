da7b4001861901af851b125031c7630ede506d28
Vinlaw <vmudehwe3661@gmail.com>
2025-01-20 22:13:54 -0500

    Add functionality to validate and parse command arguments

--- engine/EightPuzzleEngine.py
+++ engine/EightPuzzleEngine.py
@@ -69,3 +69,103 @@ class EightPuzzleEngine:
             return [Moves.DOWN, Moves.LEFT, Moves.RIGHT]
         if free_position == 8:
             return [Moves.DOWN, Moves.RIGHT]
+        
+    
+    def scramble(self, n: int, seed: int = None):
+        random_generator = random.Random(seed) if seed is not None else random
+        for _ in range(n):
+            valid_move = random_generator.choice(self.valid_moves)
+            self.move(valid_move)
+    
+    def print_state(self,state: list):
+        state_format = "\n".join(" ".join(str(state[i + j]) if state[i + j] != 0 else " " for j in range(3)) for i in range(0, 9, 3))
+        print(state_format)
+
+    def parse_command(self, command_string: str) -> Union[str, Commands] | Union[list, Commands]:
+
+        if not command_string:
+            raise CommandParsingError("Error: at least one command is required")
+
+        try:
+            # Validate the move command
+            if "move" in command_string:
+                assert len(command_string) == 2 \
+                    and command_string[0] == "move" \
+                    and command_string[1] in ["up", "down", "left", "right"] \
+                    and " ".join(command_string) in [m.value for m in self.valid_moves], \
+                    f"Error: invalid move: {' '.join(command_string)}"
+                command = moves[" ".join(command_string)]
+                print(" ".join(command_string))
+                return command, Commands.MOVE
+
+            # Validate the setState command
+            if "setState" in command_string:
+                try:
+                    state = [int(value) for value in command_string[1:]]
+                except Exception as e:
+                    raise Exception(f"Error: invalid puzzle state: {e}")
+
+                assert len(command_string) == 10 \
+                    and command_string[0] == "setState" \
+                    and set(state) == set(range(9)), \
+                    f"Error: invalid puzzle state: {' '.join(command_string)}"
+                print(" ".join(command_string))
+                return state, Commands.SET
+
+            # Validate the printState command
+            if "printState" in command_string:
+                assert len(command_string) == 1 \
+                    and command_string[0] == "printState", \
+                    f"Invalid printState command: {' '.join(command_string)}"
+                state = self.state
+                print(" ".join(command_string)) 
+                return state, Commands.PRINT
+
+            # Validate the comment command
+            if "#" in command_string or "//" in command_string:
+                assert command_string[0] in ["#", "//"], \
+                    f"Invalid comment command: {' '.join(command_string)}"
+                symbol = command_string[0]
+                comment = " ".join(command_string[1:])
+                print(" ".join(command_string))
+                return comment, Commands.COMMENT
+
+            # Validate the scramble command
+            if "scramble" in command_string:
+                if "seed" in command_string:
+                    assert len(command_string) == 4 \
+                        and command_string[0] == "scramble" \
+                        and command_string[1].isnumeric() \
+                        and command_string[2] == "seed" \
+                        and command_string[3].isnumeric(), \
+                        f"Invalid scramble command: {' '.join(command_string)}"
+                    seed = int(command_string[3])
+                    n = int(command_string[1])
+                    print(" ".join(command_string))
+                    return (n, seed), Commands.SEED
+                else:
+                    assert len(command_string) == 2 \
+                        and command_string[0] == "scramble" \
+                        and command_string[1].isnumeric(), \
+                        f"Invalid scramble command: {' '.join(command_string)}"
+                    n = int(command_string[1])
+                    print(" ".join(command_string))
+                    return n, Commands.SCRAMBLE
+
+            # Validate the file command
+            if ".txt" in command_string[0]:
+                assert len(command_string) == 1, \
+                    f"Invalid file command: {' '.join(command_string)}"
+                filename = command_string[0]
+                print(" ".join(command_string))
+                return filename, Commands.CMDFILE
+
+            else:
+                raise CommandParsingError(f"Invalid command: {' '.join(command_string)}")
+            
+
+        except AssertionError as e:
+            raise CommandParsingError(e)
+
+        except Exception as e:
+            raise Exception(f"Invalid command: {' '.join(command_string)}")
